4d1128ce0d8c246718b2a09e5c3ae8e3a76bed44
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 11:24:26 +0100

    test(mars rover): add first test: mars rover exists

--- mars_rover_mission_tests/test_mars_rover.py
+++ mars_rover_mission_tests/test_mars_rover.py
@@ -1,5 +1,12 @@
 import unittest
 
+from mars_rover_mission.mars_rover import MarsRover
+
 
 class TestMarsRover(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.mars_rover = MarsRover()
+
+    def test_mars_rover_exists(self):
+        self.assertIsInstance(self.mars_rover, MarsRover, "MarsRover class is missing or initiated incorrectly")
