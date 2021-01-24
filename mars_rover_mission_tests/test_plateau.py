b18986961b1599900a3484bea17afe6df6cde639
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 09:29:56 +0100

    test(plateau): fix tests by calling new method which allows to add only valid positions on the plateau

--- mars_rover_mission_tests/test_plateau.py
+++ mars_rover_mission_tests/test_plateau.py
@@ -11,10 +11,10 @@ class TestPlateau(unittest.TestCase):
 
     def test_add_valid_position_to_currently_occupied_positions(self):
         plateau = Plateau(5, 5)
-        plateau.currently_occupied_positions.append([3, 3])
+        plateau.add_to_currently_occupied_positions(3, 3)
         expected = [[3, 3]]
         self.assertEqual(plateau.currently_occupied_positions, expected)
 
     def test_add_invalid_position_to_currently_occupied_positions_returns_false(self):
         plateau = Plateau(5, 5)
-        self.assertFalse(plateau.currently_occupied_positions.append(3, 6))
+        self.assertFalse(plateau.add_to_currently_occupied_positions(3, 6))
