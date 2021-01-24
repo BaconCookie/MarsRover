d00bd19d641e3f52ed61234b6f866e441a2ad704
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 11:28:21 +0100

    test(mars rover): add initialization needed for a MarsRover

--- mars_rover_mission_tests/test_mars_rover.py
+++ mars_rover_mission_tests/test_mars_rover.py
@@ -1,12 +1,14 @@
 import unittest
 
 from mars_rover_mission.mars_rover import MarsRover
+from mars_rover_mission.plateau import Plateau
 
 
 class TestMarsRover(unittest.TestCase):
 
     def setUp(self):
-        self.mars_rover = MarsRover()
+        self.mars_rover = MarsRover(start_x=1, start_y=2, start_heading='N', instructions='LMLMLMLMM',
+                                    plateau=Plateau(5, 5))
 
     def tearDown(self):
         self.mars_rover = None
