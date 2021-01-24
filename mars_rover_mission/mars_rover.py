46b09cb25e874dea3840462303c4178e4c83ac5c
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 12:03:35 +0100

    fix(mars rover): fix code so that a MarsRover can only be initiated in a valid position (on plateau and not crashing into other MarsRovers)

--- mars_rover_mission/mars_rover.py
+++ mars_rover_mission/mars_rover.py
@@ -1,8 +1,10 @@
 class MarsRover:
 
     def __init__(self, start_x, start_y, start_heading, instructions, plateau):
+        self.plateau = plateau
+        if not self.plateau.add_to_currently_occupied_positions(start_x, start_y):
+            raise ValueError
         self.x = start_x
         self.y = start_y
         self.heading = start_heading
         self.instructions = instructions
-        self.plateau = plateau
