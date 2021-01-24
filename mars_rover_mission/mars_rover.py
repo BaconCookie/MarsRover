e8bf1d5d6bda21999a65f73cba8db308fe955c4d
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 11:27:11 +0100

    feat(mars rover): add initialization needed for a MarsRover

--- mars_rover_mission/mars_rover.py
+++ mars_rover_mission/mars_rover.py
@@ -1,2 +1,8 @@
 class MarsRover:
-    pass
+
+    def __init__(self, start_x, start_y, start_heading, instructions, plateau):
+        self.x = start_x
+        self.y = start_y
+        self.heading = start_heading
+        self.instructions = instructions
+        self.plateau = plateau
