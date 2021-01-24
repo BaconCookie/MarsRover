f094467067841ecdde129f0f4e7db4a8ca2996e9
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 09:32:12 +0100

    test(plateau): add setUp and tearDown methods

--- mars_rover_mission_tests/test_plateau.py
+++ mars_rover_mission_tests/test_plateau.py
@@ -5,16 +5,19 @@ from mars_rover_mission.plateau import Plateau
 
 class TestPlateau(unittest.TestCase):
 
+    def setUp(self):
+        self.plateau = Plateau(5, 5)
+
+    def tearDown(self):
+        self.plateau = None
+
     def test_plateau_exists(self):
-        plateau = Plateau(5, 5)
-        self.assertIsInstance(plateau, Plateau, "Mission class is missing or initiated incorrectly")
+        self.assertIsInstance(self.plateau, Plateau, "Mission class is missing or initiated incorrectly")
 
     def test_add_valid_position_to_currently_occupied_positions(self):
-        plateau = Plateau(5, 5)
-        plateau.add_to_currently_occupied_positions(3, 3)
+        self.plateau.add_to_currently_occupied_positions(3, 3)
         expected = [[3, 3]]
-        self.assertEqual(plateau.currently_occupied_positions, expected)
+        self.assertEqual(self.plateau.currently_occupied_positions, expected)
 
     def test_add_invalid_position_to_currently_occupied_positions_returns_false(self):
-        plateau = Plateau(5, 5)
-        self.assertFalse(plateau.add_to_currently_occupied_positions(3, 6))
+        self.assertFalse(self.plateau.add_to_currently_occupied_positions(3, 6))
