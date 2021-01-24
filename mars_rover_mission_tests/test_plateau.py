f252482723155fe471a2674505823902b0fddd1f
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 09:10:20 +0100

    test(plateau): update test to add init

--- mars_rover_mission_tests/test_plateau.py
+++ mars_rover_mission_tests/test_plateau.py
@@ -6,5 +6,5 @@ from mars_rover_mission.plateau import Plateau
 class TestPlateau(unittest.TestCase):
 
     def test_plateau_exists(self):
-        plateau = Plateau()
+        plateau = Plateau(5, 5)
         self.assertIsInstance(plateau, Plateau, "Mission class is missing or initiated incorrectly")
