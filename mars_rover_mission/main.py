a52e3c5d624cc71258da8e5f3d3c7c123bc35f24
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 09:24:02 +0100

    feat(plateau): add method to add only valid positions to list of currently occupied positions

--- mars_rover_mission/main.py
+++ mars_rover_mission/main.py
@@ -1,2 +1,3 @@
 if __name__ == '__main__':
     print('Hello World')
+    print(True and True and (True or False))
--- mars_rover_mission/plateau.py
+++ mars_rover_mission/plateau.py
@@ -1,6 +1,22 @@
 class Plateau:
 
     def __init__(self, x, y):
-        self.plateau_max_x = x
-        self.plateau_max_y = y
+        self.max_x = x
+        self.max_y = y
         self.currently_occupied_positions = []
+
+    def add_to_currently_occupied_positions(self, new_x, new_y):
+        if self.is_valid_position(new_x, new_y):
+            self.currently_occupied_positions.append([new_x, new_y])
+            return True
+        else:
+            return False
+
+    def is_valid_position(self, x, y):
+        return self.is_on_plateau(x, y) and self.causes_no_collision(x, y)
+
+    def is_on_plateau(self, x, y):
+        return x <= self.max_x and y <= self.max_y and (self.max_x >= 0 or self.max_y >= 0)
+
+    def causes_no_collision(self, x, y):
+        return [x, y] not in self.currently_occupied_positions
