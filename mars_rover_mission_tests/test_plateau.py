daecb28252fb588643769c64d20b97d73770198b
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 09:08:22 +0100

    test(plateau): add very basic first test case

--- mars_rover_mission_tests/test_plateau.py
+++ mars_rover_mission_tests/test_plateau.py
@@ -1,5 +1,10 @@
 import unittest
 
+from mars_rover_mission.plateau import Plateau
+
 
 class TestPlateau(unittest.TestCase):
-    pass
+
+    def test_plateau_exists(self):
+        plateau = Plateau()
+        self.assertIsInstance(plateau, Plateau, "Mission class is missing or initiated incorrectly")
