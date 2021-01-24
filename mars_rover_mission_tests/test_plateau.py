72e88c9d397eb271d90bf6bc1dfa5265b3536f4e
Laura Hartgers <laura.hartgers@posteo.net>
2021-01-24 09:14:47 +0100

    test(plateau): add test to add an invalid position to list of currently occupied positions. test fails as expected, since there is no code to handle this yet

--- mars_rover_mission_tests/test_plateau.py
+++ mars_rover_mission_tests/test_plateau.py
@@ -14,3 +14,7 @@ class TestPlateau(unittest.TestCase):
         plateau.currently_occupied_positions.append([3, 3])
         expected = [[3, 3]]
         self.assertEqual(plateau.currently_occupied_positions, expected)
+
+    def test_add_invalid_position_to_currently_occupied_positions_returns_false(self):
+        plateau = Plateau(5, 5)
+        self.assertFalse(plateau.currently_occupied_positions(3, 6))
